f1939272480557eb67d4443659e6216636f00ab1
binys21 <binys1121@ewhain.net>
2025-01-08 17:05:22 +0900

    feat: useReducer로 여러개 입력창 관리

--- react/src/Hooks/Info.js
+++ react/src/Hooks/Info.js
@@ -1,51 +1,54 @@
 import { useReducer, useState } from "react";
 
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value,
-  };
-}
+const reducer = (state, action) => {
+  /*
+  switch (action.type) {
+    case "name":
+      return { ...state, name: action.value };
+    case "nickname":
+      return { ...state, nickName: action.value };
+    default:
+      return state;
+  }*/
+  return { ...state, [action.type]: action.value };
+};
 
-function useInputs(initState){
-    const [state,dispatch]=useReducer(reducer,initState);
-    const handlerChange(e)=>{
-        dispatch(e.target
-        );
-        return [state,handlerChange];
-    }
-}
 const Info = () => {
-    const [state,handlerChange]=useInputs({name:'',nickname:''});
-    const {name,nickname}=state;
+  /*
+  const [name, setName] = useState("");
+  const [nickName, setNickName] = useState("");
+
+  const changeName = (e) => setName(e.target.value);
+  const changeNickName = (e) => setNickName(e.target.value);
+  */
+  const [state, dispatch] = useReducer(reducer, { name: "", nickName: "" });
+  const { name, nickName } = state;
+  const changeValue = (e) =>
+    dispatch({ type: e.target.name, value: e.target.value });
 
   return (
     <>
       <div>
-        <p>이름:{name}</p>
-        <p>별명:{nickname}</p>
+        <p>이름: {name}</p>
+        <p>별명: {nickName}</p>
       </div>
       <div>
         <p>
           이름:{" "}
-          <input
-            type="text"
-            value={name}
-            name="name"
-            onChange={handlerChange}
-          />
+          <input type="text" value={name} name="name" onChange={changeValue} />
         </p>
         <p>
           별명:{" "}
           <input
             type="text"
-            value={nickname}
+            value={nickName}
             name="nickname"
-            onChange={handlerChange}
+            onChange={changeValue}
           />
         </p>
       </div>
     </>
   );
 };
+
 export default Info;
