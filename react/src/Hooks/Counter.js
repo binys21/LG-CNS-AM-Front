82f88e2eeef475d2b1eef6005f4239f9028af548
binys21 <binys1121@ewhain.net>
2025-01-08 16:54:56 +0900

    feat: useReducer 이용한 Counter

--- react/src/Hooks/Counter.js
+++ react/src/Hooks/Counter.js
@@ -1,76 +1,37 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
-const CounterWithLocalVariable = () => {
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+const Counter = () => {
+  /*
   const [count, setCount] = useState(0);
-  let intervalId = 0;
-  const startCounter = () => {
-    intervalId = setInterval(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-    console.log(`카운터 시작...intervalId:${intervalId}`);
-  };
-  const stopCounter = () => {
-    clearInterval(intervalId);
-    console.log(`카운터 정지...intervalId:${intervalId}`);
-  };
-
+  const countPlus = () => setCount(count + 1);
+  const countMinus = () => setCount(count - 1);
+*/
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <>
-      <p>카운트: {count}</p>
-      <button onClick={startCounter}>시작</button>
-      <button onClick={stopCounter}>정지</button>
+      <div>
+        현재 카운터 값은 <b>{state.count}</b>입니다.
+      </div>
+      <div>
+        <button onClick={() => dispatch({ type: "INCREMENT" })}>
+          하나 더하기
+        </button>
+        <button onClick={() => dispatch({ type: "DECREMENT" })}>
+          하나 빼기
+        </button>
+      </div>
     </>
   );
 };
-
-const CounterWithStateVariable = () => {
-  const [count, setCount] = useState(0);
-  const [intervalId, setInterId] = useState(0);
-
-  const startCounter = () => {
-    const id = setInterval(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-    setInterId(id);
-    console.log(`카운터 시작... intervalId: ${intervalId}`);
-  };
-
-  const stopCounter = () => {
-    clearInterval(intervalId);
-    console.log(`카운터 정지... intervalId: ${intervalId}`);
-  };
-  return (
-    <>
-      <p>카운트:{count}</p>
-      <button onClick={startCounter}>시작</button>
-      <button onClick={stopCounter}>정지</button>
-    </>
-  );
-};
-
-const CounterWithRefVariable = () => {
-  const [coutn, setCount] = useState(0);
-  const intervalId = useRef(0);
-
-  const startCounter = () => {
-    intervalId.current = setInterval(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-    console.log(`카운너 시작,,,intervalId:${intervalId.current}`);
-  };
-
-  const stopCounter = () => {
-    clearInterval(intervalId.current);
-    console.log(`카운터 정지...intervalId:${intervalId.current}`);
-  };
-};
-
-export default () => {
-  return (
-    <>
-      <CounterWithLocalVariable />
-      <CounterWithStateVariable />
-      <CounterWithRefVariable />
-    </>
-  );
-};
+export default Counter;
