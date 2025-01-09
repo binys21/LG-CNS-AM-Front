72543688ca6cf3ee171fa16197bc3c5667cdf71a
binys21 <binys1121@ewhain.net>
2025-01-09 13:39:03 +0900

    feat: useNavigate와 NavLink

--- react/src/RouterDom/App.js
+++ react/src/RouterDom/App.js
@@ -3,15 +3,18 @@ import Home from "./Home";
 import About from "./About";
 import { Profiler } from "react";
 import Profiles from "./Profiles";
+import Layout from "./Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<About />} />
-        <Route path="/profiles" element={<Profiles />}>
-          <Route path=":username" element={<Profiler />}></Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<About />} />
+          <Route path="/profiles" element={<Profiles />}>
+            <Route path=":username" element={<Profiler />}></Route>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- react/src/RouterDom/Layout.js
+++ react/src/RouterDom/Layout.js
@@ -1,6 +1,7 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 
 export default function Layout() {
+  const navigate = useNavigate();
   return (
     <div>
       <header>
@@ -9,7 +10,12 @@ export default function Layout() {
             <Link to="/">홈</Link>
           </li>
           <li>
-            <Link to="/about">소개</Link>
+            <NavLink
+              to="/about"
+              style={({ isActive }) => (isActive ? { color: red } : undefined)}
+            >
+              소개
+            </NavLink>
           </li>
           <li>
             <Link to="/info">정보</Link>
@@ -18,6 +24,8 @@ export default function Layout() {
             <Link to="/profiles">프로파일</Link>
           </li>
         </ul>
+        <button onClick={() => navigate(-1)}>이전 페이지로 이동 </button>
+        <button onClick={() => navigate("/info")}>정보 페이지로 이동</button>
       </header>
       <main>
         <Outlet />
