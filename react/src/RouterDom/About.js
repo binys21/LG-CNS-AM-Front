269c69b0b7646bf24377abf9f90768ad7bcbcec5
binys21 <binys1121@ewhain.net>
2025-01-09 13:13:06 +0900

    feat: useSeearchParams() 이용하도록 수정

--- react/src/RouterDom/About.js
+++ react/src/RouterDom/About.js
@@ -1,8 +1,17 @@
+import { useLocation, useSearchParams } from "react-router-dom";
+
 const About = () => {
+  /*
+  const location = useLocation();
+  const quries = qs.parse(location.search, { ignoreQueryPrefix: true });
+  */
+  const [searchParams, setSearchParams] = useSearchParams();
+  const detail = searchParams.get("detail");
   return (
     <div>
       <h1>About</h1>
       <h2>react-route-dom example</h2>
+      {detail === "true" && <h2>상세 내역입니다.</h2>}
     </div>
   );
 };
