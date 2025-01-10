542bf919920557487728e7661dcf7acaf26d7da0
binys21 <binys1121@ewhain.net>
2025-01-10 19:12:50 +0900

    feat: 유튜브 비디오 실행 기능 추가

--- react/src/searchMovie/MovieDetail.js
+++ react/src/searchMovie/MovieDetail.js
@@ -1,12 +1,22 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+const opts = {
+  height: "390",
+  width: "640",
+  playerVars: {
+    autoplay: 1,
+  },
+};
+
 const MovieDetail = () => {
   const params = useParams();
   const movieid = params.movieid;
 
   const [videos, setVideos] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const [key, setKey] = useState("");
 
   //컴포넌트가 마운트되었을 때 비디오 클립 조회
   useEffect(() => {
@@ -23,18 +33,23 @@ const MovieDetail = () => {
 
   return (
     <>
-      {isLoading && <h1>데이터를 가져오고 있습니다.</h1>}
-      {!isLoading && videos.length === 0 && (
-        <h1>등록된 비디오 클립이 존재하지 않습니다.</h1>
-      )}
-      {!isLoading &&
-        videos.length !== 0 &&
-        videos.map((v) => (
-          <div>
-            {v.name} (출시일:{v.published_at.substring(0, 10)})
-          </div>
-        ))}
+      <div>
+        {isLoading && <h1>데이터를 가져오고 있습니다.</h1>}
+        {!isLoading && videos.length === 0 && (
+          <h1>등록된 비디오 클립이 존재하지 않습니다.</h1>
+        )}
+        {!isLoading &&
+          videos.length !== 0 &&
+          videos.map((v) => (
+            <div style={{ fontSize: 20 }} onClick={() => setKey(v.key)}>
+              {v.name} (출시일: {v.published_at.substring(0, 10)})
+            </div>
+          ))}
+      </div>
+
+      {key && <YouTube videoId={key} opts={opts} />}
     </>
   );
 };
+
 export default MovieDetail;
